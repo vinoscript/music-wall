40155108df38e083df1088a4d7875aa179ae2af0
vinoscript <wstikk@gmail.com>
2016-04-18 19:27:51 +0000

    Linked form input and added redirect

--- app/actions.rb
+++ app/actions.rb
@@ -12,4 +12,14 @@ get '/tracks/new' do
   erb :'tracks/new'
 end
 
+post '/tracks' do
+  @track = Track.new(
+    song_title: params[:song_title],
+    author: params[:author],
+    url: params[:url]
+  )
+  @track.save
+  redirect '/tracks'
+end
+
 
