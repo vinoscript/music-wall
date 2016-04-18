51a6e3e37fa5cff190b4cf079c0b5d55a60c6b9e
vinoscript <wstikk@gmail.com>
2016-04-18 21:42:20 +0000

    Added show track details page

--- app/actions.rb
+++ app/actions.rb
@@ -12,6 +12,11 @@ get '/tracks/new' do
   erb :'tracks/new'
 end
 
+get '/tracks/:id' do
+  @track = Track.find params[:id]
+  erb :'tracks/show'
+end
+
 post '/tracks' do
   @track = Track.new(
     song_title: params[:song_title],
@@ -21,5 +26,6 @@ post '/tracks' do
   @track.save
   redirect '/tracks'
 end
+
 
 
