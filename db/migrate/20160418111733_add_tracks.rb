c023c9b9015b528037218e3cd05d3e78b2b54707
vinoscript <wstikk@gmail.com>
2016-04-18 18:39:49 +0000

    Added tracks model

---
+++ db/migrate/20160418111733_add_tracks.rb
@@ -0,0 +1,12 @@
+class AddTracks < ActiveRecord::Migration
+
+  def change
+    create_table :tracks do |t|
+      t.string :song_title
+      t.string :author
+      t.string :url
+      t.timestamps
+    end
+  end
+
+end
