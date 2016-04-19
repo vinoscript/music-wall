be6bb16699a8f1ed43e800dc7436bac55110ce1f
vinoscript <wstikk@gmail.com>
2016-04-19 15:56:02 +0000

    Added login and register

--- app/actions.rb
+++ app/actions.rb
@@ -10,41 +10,9 @@ get '/tracks' do
   erb :'tracks/index'
 end
 
-get '/users/register' do
-  @user = User.new
-  erb :'users/register'
-end
-
-post '/users' do
-  @user = User.new(
-    email: params[:email],
-    password: params[:password]
-  )
-  if @user.save
-    redirect '/users'
-  else
-    erb :'users/register'
-  end
-end
-
-get '/users/login' do
-  erb :'users/login'
-end
-
-get '/users' do
-  @users = User.all
-  erb :'users/index'
-end
-
 get '/tracks/new' do
   @track = Track.new
   erb :'tracks/new'
-end
-
-get '/tracks/:id' do
-  @track = Track.find params[:id]
-  @tracks_by_author = Track.where(author: @track.author).where.not(id: params[:id])
-  erb :'tracks/show'
 end
 
 post '/tracks' do
@@ -59,6 +27,60 @@ post '/tracks' do
     erb :'tracks/new'
   end
 end
+
+get '/tracks/:id' do
+  @track = Track.find params[:id]
+  @tracks_by_author = Track.where(author: @track.author).where.not(id: params[:id])
+  erb :'tracks/show'
+end
+
+
+get '/users' do
+  @users = User.all
+  erb :'users/index'
+end
+
+get '/users/register' do
+  @user = User.new
+  @logged_in = session[:name]
+  erb :'users/register'
+end
+
+post '/users' do
+  @user = User.new(
+    email: params[:email],
+    password: params[:password]
+  )
+  if @user.save
+    session[:name] = @user.id
+    redirect '/users'
+  else
+    erb :'users/register'
+  end
+end
+
+get '/users/login' do
+  erb :'users/login'
+end
+
+post '/users/login' do
+  @user = User.find_by(email: params[:email])
+    if @user.password == params[:password]
+      session[:name] = @user.id
+      redirect '/users/details'
+    else
+      erb :'users/login'
+    end
+end
+
+get '/users/details' do
+  @user = User.find session[:name]
+  erb :'users/details'
+end
+
+
+
+
 
 
 
